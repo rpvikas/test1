f0acfd307b881dc729415923257635dd0ef982ca
rpvikas <159293331+rpvikas@users.noreply.github.com>
2024-02-11 16:20:26 +0530

    Update record_response.php

--- record_response.php
+++ record_response.php
@@ -4,8 +4,14 @@ header('Content-Type: application/json');
 // Get the user's response from the POST request
 $data = json_decode(file_get_contents('php://input'), true);
 
-// Process the response (you can save it to a database, file, etc.)
+// Process the response
 $response = $data['response'];
+
+// Store the response in a text file
+$file = 'responses.txt';
+$currentData = file_get_contents($file);
+$currentData .= $response . "\n";
+file_put_contents($file, $currentData);
 
 // Return a JSON response (optional)
 echo json_encode(['status' => 'success', 'message' => 'Response recorded successfully']);
